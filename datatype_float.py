a3ddcfdab02fd704304e087a0b0f05e5c8745dc6
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-05-02 09:55:43 +0530

    Float Data Type Example Added

---
+++ datatype_float.py
@@ -0,0 +1,14 @@
+# A number with decimal points is called as Float Data Type.
+# Floating point values can be represented only in the decimal form.
+# We don’t have binary, octal and hexadecimal forms for the float data type.
+
+f = 121.345
+print(f)
+print(type(f))
+print(id(f))
+
+# We have a special representation for Float Data Type as Exponential Form / Scientific Notation.
+fp = 1.2e3
+print(fp)
+print(type(fp))
+
