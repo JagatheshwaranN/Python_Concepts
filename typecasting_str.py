18d78f2dd1cb9b5e63d976b29a777e850b3e0ecb
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-05-05 09:53:51 +0530

    Str Type Conversion Example Added

---
+++ typecasting_str.py
@@ -0,0 +1,21 @@
+# The process of converting the one type of value to another type value is called as Type Casting.
+# str () method is used to convert other forms into str type.
+
+# Int to Str
+print(str(10))
+print(str(0b111))
+print(str(0o12))
+print(str(0x123))
+
+# Float to Str
+print(str(10.5))
+print(str(-5.5))
+
+# Complex to Str
+print(str(10+20j))
+print(str(1.5+2j))
+
+# Bool to Str
+print(str(True))
+print(str(False))
+
