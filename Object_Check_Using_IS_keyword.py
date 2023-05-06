0510cf1472e095d1e1e76b303eb374d5987cd035
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-05-06 10:59:24 +0530

    Object Check Using IS keyword Scenario Added

---
+++ Object_Check_Using_IS_keyword.py
@@ -0,0 +1,17 @@
+# In python, we can compare two objects using IS keyword.
+
+a = 10.234
+b = 10.234
+print(a is b)  # True ( Both variables using the same object only)
+
+c = True
+d = True
+print(c is d)  # True ( Both variables using the same object only)
+
+e = "code snippet"
+f = "code snippet"
+print(e is f)  # True ( Both variables using the same object only)
+
+g = 10+20j
+h = 10+20j
+print(g is h)
