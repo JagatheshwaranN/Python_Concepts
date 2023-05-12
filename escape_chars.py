1cfe3dfeeef61c047e30bdc90e6372c7e6629240
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-05-12 09:59:45 +0530

    Escape Chars Scenarios Added

---
+++ escape_chars.py
@@ -0,0 +1,17 @@
+# \n -> New Line
+# \t -> Horizontal Tab
+# \r -> Carriage Return
+# \b -> Back Space
+# \f -> Form Feed
+# \' -> Single Quote
+# \"-> Double Quote
+# \\-> Back Slash
+
+print('python program')
+print('python\tprogram')
+print('python\nprogram')
+print('This is \'symbol\' of gratitude')
+print('This is \"symbol\" of courage')
+print('python\rlanguage')
+print('python\flanguage')
+print('This is \\python\\ course')
