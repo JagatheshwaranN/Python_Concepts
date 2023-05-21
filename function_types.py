c40d598865024d9c8e63f470457a9df3a1cfe256
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-05-21 10:55:55 +0530

    Function Scenarios Updated

--- function_types.py
+++ function_types.py
@@ -2,16 +2,8 @@
 # Function can accept arguments and also return values.
 # It is created using def keyword.
 
-# Create & Call Function.
-# Function Parameters / Arguments.
-# Function Arbitrary Arguments.
-# Function Keyword Arguments.
-# Function Arbitrary Keyword Arguments.
-# Default Parameter Value.
-# Pass Any Data Types as an Argument to a Function.
-# Function with Return Value.
-# Function with Pass Statement.
 
+# Create & Call Function.
 def simple_func():
     print("Hello, I'm Python Function")
 
@@ -19,6 +11,7 @@ def simple_func():
 simple_func()
 
 
+# Function Parameters / Arguments.
 def func_wt_args(name, age):
     print("Hello {}, you are {} years old".format(name, age))
 
@@ -26,6 +19,7 @@ def func_wt_args(name, age):
 func_wt_args('John', 29)
 
 
+# Function Arbitrary Arguments.
 def func_wt_arb_args(*fruits):
     for fruit in fruits:
         print(fruit)
@@ -34,6 +28,7 @@ def func_wt_arb_args(*fruits):
 func_wt_arb_args('apple', 'orange', 'mango', 'grapes')
 
 
+# Function Keyword Arguments.
 def func_wt_keyword_args(name, age):
     message = "Hello {}, you are {} years old".format(name, age)
     print(message)
@@ -43,9 +38,41 @@ func_wt_keyword_args(name='John', age='29')
 func_wt_keyword_args(age='27', name='Alex')
 
 
+# Function Arbitrary Keyword Arguments.
 def func_wt_abr_key_args(**dataset):
     for data in dataset:
         print(data, dataset[data])
 
 
 func_wt_abr_key_args(name='John', age='29', city='New York')
+
+
+# Default Parameter Value.
+def func_wt_default_param(name, age, city='Seattle'):
+    message = "Hello {}, you are {} years old and living in {}".format(name, age, city)
+    print(message)
+
+
+func_wt_default_param('John', 29)
+
+
+# Pass Any Data Types as an Argument to a Function.
+def func_any_datatype_passed(fruits):
+    for fruit in fruits:
+        print(fruit)
+
+
+func_any_datatype_passed(['apple', 'orange', 'mango', 'grapes'])
+
+
+# Function with Return Value.
+def func_wt_return_val(name, age, city='Seattle'):
+    return "Hello {}, you are {} years old and living in {}".format(name, age, city)
+
+
+print(func_wt_return_val('John', 29))
+
+
+# Function with Pass Statement.
+def func_wt_no_content():
+    pass
