6f6dfb52cef12029ecc506c1110eb3605675ec80
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-05-22 10:02:55 +0530

    Math Module Scenarios Updated

---
+++ math_functions.py
@@ -0,0 +1,34 @@
+# Python has richset of commonly used Math operations as a ready-made functions.
+
+# Common way of using import
+import math
+
+# Second way of import
+# from math import pi,floor
+
+numbers = [20, 10, 5, 35, 70, 90, 110]
+
+# Max
+maxNum = max(numbers)
+print(maxNum)
+
+# Min
+minNum = min(numbers)
+print(minNum)
+
+# Abs
+absNum = abs(-12.45)
+print(absNum)
+
+# Pow
+powNum = pow(2, 3)
+print(powNum)
+
+# Math.PI
+print(math.pi)
+
+# Math.SQRT
+print(math.sqrt(100))
+
+# To get the help on the modules
+print(help('math'))
