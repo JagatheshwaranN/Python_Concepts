195b3a887351ad0de2bf6baa5de7f9c8ae7f5129
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-05-23 10:07:25 +0530

    UserDefined Module Scenarios Updated

---
+++ module_usage.py
@@ -0,0 +1,7 @@
+import module_userdefined
+# import module_userdefined as mu
+# from module_userdefined import greeting
+
+print(module_userdefined.COLOR)
+module_userdefined.display_color()
+module_userdefined.greeting('John')
---
+++ module_userdefined.py
@@ -0,0 +1,13 @@
+# In python, we can write separate module for different use-cases and can use it whenever needed.
+# Module can contain set of predefined functions.
+# We can develop our own user-defined module.
+
+COLOR = 'GREEN'
+
+
+def display_color():
+    print(COLOR)
+
+
+def greeting(name):
+    print("Hello", name)
