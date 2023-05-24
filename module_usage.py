a9e422fef9a0feb29805e7624fb36e6f3a50e4c1
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-05-24 10:01:34 +0530

    UserDefined Module Scenarios Updated

--- module_usage.py
+++ module_usage.py
@@ -1,3 +1,4 @@
+import math
 import module_userdefined
 # import module_userdefined as mu
 # from module_userdefined import greeting
@@ -6,5 +7,7 @@ print(module_userdefined.COLOR)
 module_userdefined.display_color()
 module_userdefined.greeting('John')
 
-# To get the list of methods and variables available in the builtin module, the below command can be used.
+# To get the list of methods and variables available in the builtin / user-defined module,
+# the below command can be used.
 print(dir(module_userdefined))
+print(dir(math))
