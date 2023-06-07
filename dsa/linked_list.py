5adc9a6189c1f0497aa0fd4808a1d4e54d95420a
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-07 19:42:12 +0530

    DSA - Linked List Implementation

--- dsa/linked_list.py
+++ dsa/linked_list.py
@@ -59,6 +59,54 @@ class LinkedList:
                 current = current.next_node
         return None
 
+    def insert(self, data, index):
+        """
+        Inserts a new Node containing data at index position
+        Insertion takes O(1) time but finding the node at the insertion point takes O(n) time
+        Takes overall O(n) time
+        """
+        if index == 0:
+            self.add(data)
+
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = current.next_node
+                position -= 1
+
+            prev_node = current
+            next_node = current.next_node
+
+            prev_node.next_node = new
+            new.next_node = next_node
+
+    def remove(self, key):
+        """
+        Removes the node containing data that matches the key
+        Returns the node or None if the key doesn't exist
+        Takes O(n) time
+        """
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+
+        return current
+
     def __repr__(self):
         nodes = []
         current = self.head
@@ -93,3 +141,9 @@ print(lst1.size())
 print(lst1)
 ele = lst1.search(20)
 print(ele)
+print("=====================================")
+lst1.insert(25, 2)
+print(lst1)
+print("=====================================")
+lst1.remove(20)
+print(lst1)
