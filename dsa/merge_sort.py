1213324c356b517cb0a418f76f13867fc85b00f0
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-08 20:54:14 +0530

    DSA - Merge Sort

---
+++ dsa/merge_sort.py
@@ -0,0 +1,71 @@
+def merge_sort(lst):
+    """
+    Sorts a list in ascending order
+    Returns a new sorted list
+
+    Divide: Find the midpoint of the list and divide into sub-lists
+    Conquer: Recursively sort the sub-lists created in the previous step
+    Combine: Merge the sorted sub-lists created in previous step
+    """
+    if len(lst) <= 1:
+        return lst
+
+    left_half, right_half = split(lst)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
+
+
+def split(lst):
+    """
+    Divide the unsorted list at midpoint into sub-lists
+    Returns two sub-lists - left and right
+    """
+    mid = len(lst) // 2
+    left = lst[:mid]
+    right = lst[mid:]
+
+    return left, right
+
+
+def merge(left, right):
+    """
+    Merges two lists (arrays), sorting them in the process
+    Returns a new merged list
+    """
+    lt = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            lt.append(left[i])
+            i += 1
+        else:
+            lt.append(right[j])
+            j += 1
+
+    while i < len(left):
+        lt.append(left[i])
+        i += 1
+    while j < len(right):
+        lt.append(right[j])
+        j += 1
+
+    return lt
+
+
+def verify_sorted(lst):
+    n = len(lst)
+
+    if n == 0 or n == 1:
+        return True
+
+    return lst[0] < lst[1] and verify_sorted(lst[1:])
+
+
+alist = [50, 23, 67, 87, 72, 94, 17, 33, 42]
+lstVal = merge_sort(alist)
+print(verify_sorted(alist))
+print(verify_sorted(lstVal))
