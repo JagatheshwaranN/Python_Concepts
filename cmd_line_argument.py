6a68afb9583e824cbbbab1540d8b77c0b3101fa8
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-07-20 10:09:59 +0530

    Python - User Input & Cmd Line Arguments

--- cmd_line_argument.py
+++ cmd_line_argument.py
@@ -1,7 +1,11 @@
 from sys import argv
-print(argv)
-print(argv[1:])
-print(argv[0])
-print(argv[1])
-print(argv[2])
-print(argv[3])
+
+# argv is a commandline argument used to take the input from command line. It treats the value
+# as Str type. It is used to separate the values using the space. If the input is having space
+# then compulsory we should enclose the value inside the double quotes.
+
+print("The number of cmdline arguments ", len(argv))
+print("The list of cmdline arguments ", argv)
+print("The cmdline arguments one by one")
+for arg in argv:
+    print(arg)
