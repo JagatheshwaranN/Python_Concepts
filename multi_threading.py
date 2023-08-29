1b4d1b1a0b4b6d3d72b613afc9ba625eb3daa010
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-08-29 10:18:00 +0530

    Python - MultiThreading

--- multi_threading.py
+++ multi_threading.py
@@ -5,9 +5,36 @@ print(threading.current_thread().getName())
 
 
 def display():
-    print("The display function is executed by Thread :", current_thread().getName()+"\n")
+    print("The display function is executed by Thread :", current_thread().getName() + "\n")
 
 
 thread = Thread(target=display)
 thread.start()
-print("This code is executed by Thread :", current_thread().getName()+"\n")
+print("This code is executed by Thread :", current_thread().getName() + "\n")
+
+
+# Create Thread by extending the thread class
+class MyThread(Thread):
+    def run(self):
+        for j in range(10):
+            print("Child Thread")
+
+
+t = MyThread()
+t.start()
+for i in range(10):
+    print("Main Thread")
+
+
+# Create Thread without using any class
+class MyThread1:
+    def show(self):
+        for j in range(10):
+            print("Child Thread2 : ", current_thread().getName()+"\n")
+
+
+obj = MyThread1()
+t2 = Thread(target=obj.show)
+t2.start()
+for i in range(10):
+    print("Main Thread2 : ", current_thread().getName() + "\n")
